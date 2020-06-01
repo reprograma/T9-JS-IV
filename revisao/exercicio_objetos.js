80e2d95b1ebedc34f39b90643231d5487f59be16
oskojess <jessica_osko@hotmail.com>
2020-06-01 15:29:58 -0300

    exercicio de objetos2

---
+++ revisao/exercicio_objetos.js
@@ -0,0 +1,15 @@
+// Analise o objeto produtos e ajuste os erros.
+
+let produtos = {
+  videos: {
+    comedia: ["comedia1""comedia2"];
+    romance ["romance1", "romance2"]
+  },
+  revistas: [
+    moda = ["lalala", "lililili"],
+    saude = ["lalala", "lililili"],
+  ]
+  jogos {
+    rpg ["rpg1", "rpg2", rpg3],
+    acao: ["acao1", "God of War"]
+  }
---
+++ revisao/objetos.js
@@ -0,0 +1,15 @@
+const pessoa = {
+  personalidade: ['personalide1', 'personalide2'],
+  areaAtuacao: 'Biologa'
+}
+
+pessoa.personalidade[0];
+
+console.log("Personalidade1: ", pessoa.personalidade[0]);
+
+
+//exercício
+
+
+
+
